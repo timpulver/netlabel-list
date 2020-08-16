61db444c440e8fd7cff78e8ea6cf84925b322114
Mike Gregoire <mgregoire@nvzion.com>
2020-08-15 21:56:39 -0400

    Add genre / country list scripts

---
+++ scripts/utils/count.js
@@ -0,0 +1,35 @@
+const data = require('../../netlabels.json');
+const sort = require('./sort');
+const items = [];
+
+const hasItem = item => {
+    let found = false;
+
+    items.map(i => {
+        if (i.name === item) {
+            found = true;
+        }
+    });
+
+    return found;
+};
+
+const pushItem = item => {
+    if (hasItem(item)) {
+        items.map(i => {
+            i.count += 1;
+        });
+    } else {
+        items.push({ name: item, count: 1 });
+    }
+};
+
+module.exports = toCount => {
+    sort(data.netlabels, toCount).map(item => {
+        pushItem(item);
+    });
+
+    items.map(item => {
+        console.log(`${item.name} (${item.count})`);
+    });
+};
